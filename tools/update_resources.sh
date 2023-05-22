23deb8ad4df3d390de231507387c61f4012e0689
Franco Cipollone <franco.c@ekumenlabs.com>
2023-05-22 14:26:27 -0300

    Deal with relative positions.
    Signed-off-by: Franco Cipollone <franco.c@ekumenlabs.com>

--- tools/update_resources.sh
+++ tools/update_resources.sh
@@ -5,19 +5,19 @@
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # Generates straight_forward resources
-WIDTH=3 CROSSWALK=True empy3 ${script_dir}/../templates/straight_forward.xml.em > resources/straight_forward/straight_forward_3m_width_crosswalk.xodr
-WIDTH=3.5 CROSSWALK=True empy3 ${script_dir}/../templates/straight_forward.xml.em > resources/straight_forward/straight_forward_3_5m_width_crosswalk.xodr
-WIDTH=4 CROSSWALK=True empy3 ${script_dir}/../templates/straight_forward.xml.em > resources/straight_forward/straight_forward_4m_width_crosswalk.xodr
+WIDTH=3 CROSSWALK=True empy3 ${script_dir}/../templates/straight_forward.xml.em > ${script_dir}/../resources/straight_forward/straight_forward_3m_width_crosswalk.xodr
+WIDTH=3.5 CROSSWALK=True empy3 ${script_dir}/../templates/straight_forward.xml.em > ${script_dir}/../resources/straight_forward/straight_forward_3_5m_width_crosswalk.xodr
+WIDTH=4 CROSSWALK=True empy3 ${script_dir}/../templates/straight_forward.xml.em > ${script_dir}/../resources/straight_forward/straight_forward_4m_width_crosswalk.xodr
 
-WIDTH=3 CROSSWALK=False empy3 ${script_dir}/../templates/straight_forward.xml.em > resources/straight_forward/straight_forward_3m_width.xodr
-WIDTH=3.5 CROSSWALK=False empy3 ${script_dir}/../templates/straight_forward.xml.em > resources/straight_forward/straight_forward_3_5m_width.xodr
-WIDTH=4 CROSSWALK=False empy3 ${script_dir}/../templates/straight_forward.xml.em > resources/straight_forward/straight_forward_4m_width.xodr
+WIDTH=3 CROSSWALK=False empy3 ${script_dir}/../templates/straight_forward.xml.em > ${script_dir}/../resources/straight_forward/straight_forward_3m_width.xodr
+WIDTH=3.5 CROSSWALK=False empy3 ${script_dir}/../templates/straight_forward.xml.em > ${script_dir}/../resources/straight_forward/straight_forward_3_5m_width.xodr
+WIDTH=4 CROSSWALK=False empy3 ${script_dir}/../templates/straight_forward.xml.em > ${script_dir}/../resources/straight_forward/straight_forward_4m_width.xodr
 
 # Generates intersection resources
-WIDTH=3 CROSSWALK=True CROSSWALK_LENGTH=2 empy3 ${script_dir}/../templates/intersection.xml.em > resources/intersection/intersection_3m_width_crosswalk.xodr
-WIDTH=3.5 CROSSWALK=True CROSSWALK_LENGTH=2 empy3 ${script_dir}/../templates/intersection.xml.em > resources/intersection/intersection_3_5m_width_crosswalk.xodr
-WIDTH=4 CROSSWALK=True CROSSWALK_LENGTH=2 empy3 ${script_dir}/../templates/intersection.xml.em > resources/intersection/intersection_4m_width_crosswalk.xodr
+WIDTH=3 CROSSWALK=True CROSSWALK_LENGTH=2 empy3 ${script_dir}/../templates/intersection.xml.em > ${script_dir}/../resources/intersection/intersection_3m_width_crosswalk.xodr
+WIDTH=3.5 CROSSWALK=True CROSSWALK_LENGTH=2 empy3 ${script_dir}/../templates/intersection.xml.em > ${script_dir}/../resources/intersection/intersection_3_5m_width_crosswalk.xodr
+WIDTH=4 CROSSWALK=True CROSSWALK_LENGTH=2 empy3 ${script_dir}/../templates/intersection.xml.em > ${script_dir}/../resources/intersection/intersection_4m_width_crosswalk.xodr
 
-WIDTH=3 CROSSWALK=False CROSSWALK_LENGTH=2 empy3 ${script_dir}/../templates/intersection.xml.em > resources/intersection/intersection_3m_width.xodr
-WIDTH=3.5 CROSSWALK=False CROSSWALK_LENGTH=2 empy3 ${script_dir}/../templates/intersection.xml.em > resources/intersection/intersection_3_5m_width.xodr
-WIDTH=4 CROSSWALK=False CROSSWALK_LENGTH=2 empy3 ${script_dir}/../templates/intersection.xml.em > resources/intersection/intersection_4m_width.xodr
+WIDTH=3 CROSSWALK=False CROSSWALK_LENGTH=2 empy3 ${script_dir}/../templates/intersection.xml.em > ${script_dir}/../resources/intersection/intersection_3m_width.xodr
+WIDTH=3.5 CROSSWALK=False CROSSWALK_LENGTH=2 empy3 ${script_dir}/../templates/intersection.xml.em > ${script_dir}/../resources/intersection/intersection_3_5m_width.xodr
+WIDTH=4 CROSSWALK=False CROSSWALK_LENGTH=2 empy3 ${script_dir}/../templates/intersection.xml.em > ${script_dir}/../resources/intersection/intersection_4m_width.xodr
