65f8475bdfb9beb0147ac2a67b7422290aaa11a6
Agustin Alba Chicar <ag.albachicar@ekumenlabs.com>
2025-08-21 15:14:14 +0200

    Adds dual_opposing_dedicated_right_turn_lanes map (#36)
    * Adds a crossing with a lane turning to the right.
    
    Signed-off-by: Agustin Alba Chicar <ag.albachicar@gmail.com>
    
    * Addresses Franco's comments.
    
    Signed-off-by: Franco Cipollone <franco.c@ekumenlabs.com>
    
    * Renames to dual_opposing_dedicated_right_turn_lanes.
    
    Signed-off-by: Franco Cipollone <franco.c@ekumenlabs.com>
    
    * Modify road names to creat 2 ids in the middle.
    
    Signed-off-by: Franco Cipollone <franco.c@ekumenlabs.com>
    
    * Leave only one lane from road 2.
    
    Signed-off-by: Franco Cipollone <franco.c@ekumenlabs.com>
    
    * Adds N-S lane for leftest lane.
    
    Signed-off-by: Franco Cipollone <franco.c@ekumenlabs.com>
    
    * Adds North - West connection.
    
    Signed-off-by: Franco Cipollone <franco.c@ekumenlabs.com>
    
    * Adds short turn roads.
    
    Signed-off-by: Franco Cipollone <franco.c@ekumenlabs.com>
    
    * Adds 300m extensions length
    
    Signed-off-by: Franco Cipollone <franco.c@ekumenlabs.com>
    
    ---------
    
    Signed-off-by: Agustin Alba Chicar <ag.albachicar@gmail.com>
    Signed-off-by: Franco Cipollone <franco.c@ekumenlabs.com>
    Co-authored-by: Franco Cipollone <franco.c@ekumenlabs.com>

--- tools/update_resources.sh
+++ tools/update_resources.sh
@@ -64,3 +64,7 @@ ${script_dir}/extract_geojson_info.sh ${script_dir}/../resources/dedicated_south
 
 # Generates curved_connected_roads resources
 empy3 ${script_dir}/../templates/curved_connected_roads.xml.em > ${script_dir}/../resources/curved_connected_roads/curved_connected_roads_default.xodr
+
+# Generates dual_opposing_dedicated_right_turn_lanes resources
+EXTENSIONS_LENGTH=300 WIDTH=4.0 STOPLINE=True RADIUS=6.0 empy3 ${script_dir}/../templates/dual_opposing_dedicated_right_turn_lanes.xml.em > ${script_dir}/../resources/dual_opposing_dedicated_right_turn_lanes/dual_opposing_dedicated_right_turn_lanes.xodr
+${script_dir}/extract_geojson_info.sh ${script_dir}/../resources/dual_opposing_dedicated_right_turn_lanes/dual_opposing_dedicated_right_turn_lanes.xodr
